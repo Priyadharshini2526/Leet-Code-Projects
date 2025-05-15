4f742e0543789a6f98d07e3db5b625f36e4f7bf8
Priyadharshini2526 <dharshinipriya26.2005@gmail.com>
2025-05-15 14:37:46 +0530

    first commit

---
+++ LetterCombination.java
@@ -0,0 +1,27 @@
+package Leetcode;
+
+import java.util.*;
+
+public class LetterCombination {
+    static final String[] KEYPAD = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public static List<String> letterCombination(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits.isEmpty()) return result;
+        backtrack(result, digits, 0, new StringBuilder());
+        return result;
+    }
+
+    private static void backtrack(List<String> res, String d, int i, StringBuilder sb) {
+        if (i == d.length()) { res.add(sb.toString()); return; }
+        for (char c : KEYPAD[d.charAt(i) - '0'].toCharArray()) {
+            backtrack(res, d, i + 1, sb.append(c));
+            sb.setLength(sb.length() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(letterCombination("23"));
+    }
+}
+
